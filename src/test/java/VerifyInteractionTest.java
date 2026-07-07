93f7c4cc7687d7ebf90bc11e121512b04df13567
GANDEPRANEETH <gandepraneeth074@gmail.com>
2026-07-07 22:06:24 +0530

    Complete Mockito exercises

---
+++ src/test/java/VerifyInteractionTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.*;
+
+public class VerifyInteractionTest {
+    @Test
+    public void testVerifyInteraction() {
+
+        ExternalApi mockApi = Mockito.mock(ExternalApi.class);
+        MyService service = new MyService(mockApi);
+
+
+        service.fetchData();
+
+
+        verify(mockApi).getData();
+    }
+}
